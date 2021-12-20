6b49a5ae6fe88c14d442db481ae40955c07caafa
vaskania26 <vnikabadze@unisens.ge>
2021-12-20 15:14:18 +0400

    added await for db

--- src/db/db.js
+++ src/db/db.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 const logger = require('../log/logger');
 
-const connectDB = () =>
-  mongoose
-    .connect(process.env.DB_URI)
-    .then(() => {
-      logger.info('Connected to db');
-    })
-    .catch((error) => {
-      logger.error(error);
-    });
-const closeDB = () => {
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URI);
+    logger.info('Connected to db');
+  } catch (error) {
+    logger.error(error);
+  }
+};
+
+const closeDB = async () => {
   logger.info('DB closed');
-  mongoose.connection.close();
+  await mongoose.connection.close();
 };
 
 module.exports = { connectDB, closeDB };
